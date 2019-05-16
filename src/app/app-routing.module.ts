14f68a748f9538dae626603c33aa9c09fcc5b624
Siwat Techavoranant <keen@keendev.net>
2019-05-17 01:48:01 +0700

    feat: implement new register page
    PR Close #80

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from 'app/home/home.component';
-import {ViewWreathComponent} from './view-wreath/view-wreath.component';
 
 const routes: Routes = [
   {
@@ -11,16 +10,6 @@ const routes: Routes = [
         path: '',
         pathMatch: 'full',
         component: HomeComponent
-      }
-    ]
-  },
-  {
-    path: 'wreath',
-    children: [
-      {
-        path: '',
-        pathMatch: 'full',
-        component: ViewWreathComponent
       }
     ]
   }
