fcef3ed262f1661c288ab3ee03e340382637a36a
Siwat Techavoranant <keen@keendev.net>
2019-05-17 13:10:53 +0700

    Implement new register page

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { AngularFireDatabase } from '@angular/fire/database';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
+import {AngularFireDatabase} from '@angular/fire/database';
 import * as firebase from 'firebase/app';
 import 'firebase/database';
 import {Queue} from '../queue';
-import {map, switchMap} from 'rxjs/operators';
 import {Observable} from 'rxjs';
+import {KeyValue} from '@angular/common';
 
 @Component({
   selector: 'and-register',
@@ -15,13 +15,15 @@ import {Observable} from 'rxjs';
 export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
   $queues: Observable<Queue>;
+  reserved: KeyValue<string, any>;
 
-  constructor(private afd: AngularFireDatabase) {}
+  constructor(private afd: AngularFireDatabase) {
+  }
 
   ngOnInit() {
     this.registerForm = new FormGroup({
       name: new FormControl('', Validators.required),
-      read: new FormControl('', Validators.required),
+      read: new FormControl(''),
       table: new FormControl('', Validators.required),
       wreath: new FormControl('', Validators.required),
       tel: new FormControl(''),
@@ -30,24 +32,38 @@ export class RegisterComponent implements OnInit {
     });
     this.$queues = this.afd
       .object<Queue>('queues/-1')
-      .snapshotChanges();
+      .valueChanges();
+  }
+
+  showRegister(item: KeyValue<string, any>) {
+    this.reserved = item;
+    this.registerForm.patchValue(item.value.value);
   }
 
   submit() {
     if (this.registerForm.valid) {
       // Push on to first queue
-      const ref = this.afd.database
+      const newKey = this.afd.database
         .ref(`queues/0/queue`)
-        .push({
-          value: this.registerForm.value,
-          registerTime: firebase.database.ServerValue.TIMESTAMP
-        });
+        .push().key;
+      const ref = this.afd.database
+        .ref(`queues/0/queue/${newKey}`);
+      ref.set({
+        value: this.registerForm.value,
+        registerTime: firebase.database.ServerValue.TIMESTAMP,
+        keyStack: {[newKey]: this.reserved.key}
+      });
       ref
         .child('timeStack')
         .push(firebase.database.ServerValue.TIMESTAMP)
         .then(data => {
           this.registerForm.reset();
         });
+
+      // Remove from queue -1
+      this.afd.database.ref(`queues/-1/queue/${this.reserved.key}`).remove();
+
+      this.reserved = null;
     }
   }
 }
