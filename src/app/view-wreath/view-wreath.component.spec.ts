049680474dc1454849ec51947482cf903d99b1df
Sarun Intaralwan <sarunint@docchula.com>
2021-02-07 05:36:06 +0700

    chore(deps): update angular and angular cli packages

--- src/app/view-wreath/view-wreath.component.spec.ts
+++ src/app/view-wreath/view-wreath.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ViewWreathComponent } from './view-wreath.component';
 
@@ -6,7 +6,7 @@ describe('ViewWreathComponent', () => {
   let component: ViewWreathComponent;
   let fixture: ComponentFixture<ViewWreathComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ViewWreathComponent ]
     })
